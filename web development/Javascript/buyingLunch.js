f3548e243756bb28f764bc0f965fdbb84b8fa2fc
Hayden-Neilson <haydenneilson5@gmail.com>
2021-02-01 07:52:20 -0700

    bmi lovCalc and buyingLunch

---
+++ web development/Javascript/buyingLunch.js
@@ -0,0 +1,6 @@
+function whosPaying(names) {
+        
+    var randomName = names[Math.floor(Math.random() * names.length)];
+    
+    return randomName + " is going to buy lunch today!";   
+    }
--- web development/Javascript/love calculator.js
+++ web development/Javascript/love calculator.js
@@ -1,3 +1,14 @@
 var loveScore = Math.random()* 100
 loveScore = Math.floor(loveScore) + 1
 console.log(loveScore + '%')
+
+// var loveScore = Math.random()* 100
+// loveScore = Math.floor(loveScore) + 1
+// if   (loveScore <= 75) {
+//   console.log("you will do great)together")
+//   else if  (loveScore >= 50) {
+//   console.log("you may not make it");
+//   } 
+//   else (loveScore <= 10) { 
+//   console.log("best not try");
+//   }
